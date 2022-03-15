2227fe51e81c31d332f5a0dfb518cdd2f173e41a
Esteban <estebancoloradogonzalez@gmail.com>
2022-03-14 20:28:00 -0500

    implementación del ultimo codigo

--- src/main/java/com/trm/TrmApplication.java
+++ src/main/java/com/trm/TrmApplication.java
@@ -1,13 +1,13 @@
-package com.uco.myproject;
+package com.trm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class TrmApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(TrmApplication.class, args);
 	}
 
 }
--- src/main/java/com/uco/myproject/infraestructura/error/Error.java
+++
@@ -1,3 +0,0 @@
-package com.uco.myproject.infraestructura.error;
-
-record Error(String nombreExcepcion, String mensaje) { }
